8123d1c9a29a92b5af1553bcf74e9e32bc9671b4
Frames-th <meow.thelittle@gmail.com>
2019-07-23 13:05:20 +0700

    modify some code in Lab2_2

--- src/Lab2/Lab2_2.java
+++ src/Lab2/Lab2_2.java
@@ -1,11 +1,13 @@
 package Lab2;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Lab2_2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
 
         System.out.print("Input Your name : ");
         String a = scanner.nextLine();
@@ -16,7 +18,7 @@ public class Lab2_2 {
         System.out.println("Hello " +a+ " " +b);
 
         System.out.print("Please, input Your ID Card : ");
-        double c = scanner1.nextDouble();
+        BigInteger c = scanner1.nextBigInteger();
 
         System.out.print("Please, input Your address : ");
         String d = scanner.nextLine();
@@ -25,12 +27,13 @@ public class Lab2_2 {
         String e = scanner.nextLine();
 
         System.out.print("Please, input Your phone number : ");
-        double f = scanner1.nextDouble();
+        BigInteger f = scanner1.nextBigInteger();
 
-        System.out.println("So, your name is " +a+b +", your ID Card number " );
+        System.out.println("So, your name is " +a+ " " +b +", your ID Card number " );
+
         System.out.println(c);
         System.out.println(", your address at " +d);
-        System.out.println(", your E=mail is " +e +"and your phone number is ");
+        System.out.println(", your E-mail is " +e +"and your phone number is ");
         System.out.println(f);
         System.out.println("Thank you !");
 
